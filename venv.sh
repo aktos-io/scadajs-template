ff1f2b25873e795f58c32c844fd1b320d71c4bad
Cerem Cem ASLAN <cem@aktos.io>
2020-08-04 11:20:28 +0300

    cleanup

--- venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-venv_path=$DIR/scada.js/nodeenv
-if [[ ! -d $venv_path ]] && [[ -n $SCADAJS_1_ENV ]]; then
-    venv_path=$SCADAJS_1_ENV
-fi
-[[ -z $sname ]] && { echo "Session name is empty"; exit 1; }
-[[ -z $venv_path ]] && { echo "VIRTUAL_ENV path is empty"; exit 1; }
-tmux_set_venv $sname $venv_path
